d1142ac886ba50e1e1c7ad7c8ecf52f6e209e5de
personal <suhaillahmadd0@gmail.com>
2023-10-27 00:55:47 +0530

    Added column,box layout

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,8 +23,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,29 +41,36 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, widthDp = 200, heightDp = 300)
 @Composable
 private fun PreviewFunction() {
-    TextField(
-        value = "Some Test Value",
+    TextField(value = "Some Test Value",
         onValueChange = {},
-        label = { Text(text = "Enter some text") }
-    )
+        label = { Text(text = "Enter some text") })
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EnterText() {
     Column(
-        modifier = Modifier.fillMaxSize().padding(100.dp),
-        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(100.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val state = remember{mutableStateOf("")}
-        TextField(
-            value = state.value,
-            onValueChange = {
-                state.value = it
-            },
-            label = { Text(text = "Enter some text") }
-        )
+        val state = remember { mutableStateOf("") }
+        TextField(value = state.value, onValueChange = {
+            state.value = it
+        }, label = { Text(text = "Enter some text") })
+
+        Text(text = "Hello this is a text", fontSize = 22.sp)
+
+        Box(Modifier.size(300.dp, 300.dp), contentAlignment = Alignment.Center) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.test_img
+                ), contentDescription = "this is a test image"
+            )
+            Text(text = "This is text over test image")
+        }
     }
 }
 
